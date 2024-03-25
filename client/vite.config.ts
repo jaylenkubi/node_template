10d61ee551ecb891d01b68a255540b3319f3307d
Jaylen_Script_Assist <kwasi@scriptassist.co.uk>
2024-03-25 12:11:37 +0000

    changes to docker file

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
 	server: {
 		port: 5173
 	},
 	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@ui-services': path.resolve(__dirname, '../libraries/services/ui-services'),
+		},
+	},
 	css: {
 		preprocessorOptions: {
 			scss: {
